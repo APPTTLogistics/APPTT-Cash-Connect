cf6e5fe422047972880b64cffecfdf01e7eb73b6
APPTTLogistics <apptransportandtravels@gmail.com>
2025-07-06 13:06:38 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -10,8 +10,6 @@ $(document).ready(function() {
         autoplaySpeed: 4000,
         fade: true,
         cssEase: 'linear',
-        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
     });
     
     // Timeline animation
