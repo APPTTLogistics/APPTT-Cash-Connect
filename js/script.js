2737b96e0997ac21b90aa59e71ab61e6ff2fcf55
APPTTwebsite <apptransportandtravels@gmail.com>
2025-07-16 11:44:50 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -131,14 +131,7 @@ document.addEventListener("DOMContentLoaded", () => {
     navLinks.classList.toggle("show");
   });
 });
-window.addEventListener('scroll', function () {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-      navbar.style.backgroundColor = '#f0fef0'; // or any color
-    } else {
-      navbar.style.backgroundColor = 'transparent';
-    }
-  });
+
   dots.forEach((dot) => {
   dot.addEventListener('mouseenter', () => {
     const popup = dot.querySelector('.timeline-popup');
